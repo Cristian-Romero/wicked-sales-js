b52951eef540c677d0e50499c113a0de0b154282
Cristian Romero <crisromero02@outlook.com>
2020-06-12 22:12:41 +0000

    Created product-list.jsx module that has a state of an empty array to the products property

---
+++ client/components/product-list.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      products: []
+    };
+  }
+
+}
+
+export default ProductList;
