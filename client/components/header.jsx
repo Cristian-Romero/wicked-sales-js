b381d66f6036a72a0b11515c8f910568de17d556
Cristian Romero <crisromero02@outlook.com>
2020-06-12 20:46:02 +0000

    Created a header.jsx module and defined a Header class component within it. It renders a react element of type header and inside the header there is a react element of type icon and div

---
+++ client/components/header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <i className="fas fa-dollar-sign"></i>
+        <div>Wicked Sales</div>
+      </header>
+    );
+  }
+}
+
+export default Header;
