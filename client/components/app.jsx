46df96c010674cd597f71597407ab8c1cdb7651b
Cristian Romero <crisromero02@outlook.com>
2020-06-12 20:36:05 +0000

    Defined a new App class in app.jsx

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,26 +1,7 @@
-
 import React from 'react';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: null,
-      isLoading: true
-    };
-  }
+class App extends React.Component {
 
-  componentDidMount() {
-    fetch('/api/health-check')
-      .then(res => res.json())
-      .then(data => this.setState({ message: data.message || data.error }))
-      .catch(err => this.setState({ message: err.message }))
-      .finally(() => this.setState({ isLoading: false }));
-  }
-
-  render() {
-    return this.state.isLoading
-      ? <h1>Testing connections...</h1>
-      : <h1>{this.state.message}</h1>;
-  }
 }
+
+export default App;
