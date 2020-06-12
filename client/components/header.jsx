159c7bf27c6a00d93fb1ba7d9793b9854d945fc4
Cristian Romero <crisromero02@outlook.com>
2020-06-12 21:54:03 +0000

    Added styling using via bootstrap on Header render component

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -3,9 +3,11 @@ import React from 'react';
 class Header extends React.Component {
   render() {
     return (
-      <header>
-        <i className="fas fa-dollar-sign"></i>
-        <div>Wicked Sales</div>
+      <header className="ws-header">
+        <div className="ml-5 h-100 d-flex align-items-center">
+          <i className="fas fa-dollar-sign mr-1"></i>
+          <h6 className="mb-0">Wicked Sales</h6>
+        </div>
       </header>
     );
   }
