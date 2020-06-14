0210ef903504bd72e7572cfffd871291a07767d3
Cristian Romero <crisromero02@outlook.com>
2020-06-14 21:04:05 +0000

    Completed the ProductDetails render method to render a view of the products details after the details are loaded from the server

--- client/components/product-details.jsx
+++ client/components/product-details.jsx
@@ -8,17 +8,42 @@ class ProductDetails extends React.Component {
     };
   }
 
-  componentDidMount(productId) {
-    fetch(`/api/products/${productId}`)
+  componentDidMount() {
+    fetch(`/api/products/${this.props.productId}`)
       .then(response => response.json())
-      .then(product => product[productId])
+      .then(data => {
+        this.setState({
+          product: data
+        });
+      })
       .catch(err => {
         console.error(err);
       });
   }
 
   render() {
-    return null;
+    return !this.state.product
+      ? (
+        <div className="container">
+          <div className="card col-12">
+            <div className="row text-muted card-body">
+              &lt; Back to catalog
+            </div>
+            <div className="row">
+              <div className="col">
+                <img src={ this.state.product.image } alt="" />
+              </div>
+              <div className="col">
+                <h2 className="card-title">{ this.state.product.name }</h2>
+                <p className="card-text text-muted">{ this.state.product.price }</p>
+                <p className="card-text">{ this.state.product.shortDescription }</p>
+              </div>
+            </div>
+            <div className="row card-text card-body">{this.state.product.longDescription }</div>
+          </div>
+        </div>
+      )
+      : null;
   }
 }
 
