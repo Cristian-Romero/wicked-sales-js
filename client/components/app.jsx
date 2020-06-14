0cf81298fc5735e8d9f6b132cb7b0420a1bc9b33
Cristian Romero <crisromero02@outlook.com>
2020-06-14 21:12:57 +0000

    Added a property to the App component named view thats value is an object with two properties named name and params. Whos values are a string named catalog and an epmty object

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -3,6 +3,16 @@ import Header from './header';
 import ProductList from './product-list';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      view: {
+        name: 'catalog',
+        params: {}
+      }
+    };
+  }
+
   render() {
     return (
       <div>
