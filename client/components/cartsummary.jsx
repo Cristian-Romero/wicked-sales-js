a1ce3773dee795fb9676141eb667decfc598dbfb
Cristian Romero <crisromero02@outlook.com>
2020-07-03 23:56:31 +0000

    Add a render method that returns aa react element of type div. It add a Back to Catalog button and a My Cart title.

--- client/components/cartsummary.jsx
+++ client/components/cartsummary.jsx
@@ -2,6 +2,19 @@ import React from 'react';
 
 class CartSummary extends React.Component {
 
+  render() {
+    return (
+      <div className="container my-5 mx-auto">
+        <div className="row col-3 text-muted card-body back-to-catalog"
+          onClick={() => this.props.setView('catalog', {})}>
+          &lt; Back to catalog</div>
+        <div>
+          <h1>My Cart</h1>
+        </div>
+
+      </div>
+    );
+  }
 }
 
 export default CartSummary;
