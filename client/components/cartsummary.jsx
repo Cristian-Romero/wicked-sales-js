936e6ed54c013d0b2a11d576492866cb10bf958a
Cristian Romero <crisromero02@outlook.com>
2020-07-04 00:11:19 +0000

    Add an if conditional that evaluates this.props.items.length. If it is true it returns all the items in your cart. Otherwise it displays a message letting you know there are no items in your cart.

--- client/components/cartsummary.jsx
+++ client/components/cartsummary.jsx
@@ -4,25 +4,35 @@ import CartSummaryItem from './cartsummaryitem';
 class CartSummary extends React.Component {
 
   render() {
+    if (this.props.items.length) {
+      return (
+        <div className="container my-5 mx-auto">
+          <div className="row col-3 text-muted card-body back-to-catalog"
+            onClick={() => this.props.setView('catalog', {})}>
+            &lt; Back to catalog</div>
+          <div>
+            <h1>My Cart</h1>
+          </div>
+          <div>
+            {this.props.items.map(item => {
+              return (<CartSummaryItem
+                key={item.productId}
+                image={item.image}
+                name={item.name}
+                price={item.price}
+                shortDescription={item.shortDescription}
+              />);
+            })}
+          </div>
+          <div>
+
+          </div>
+        </div>
+      );
+    }
     return (
-      <div className="container my-5 mx-auto">
-        <div className="row col-3 text-muted card-body back-to-catalog"
-          onClick={() => this.props.setView('catalog', {})}>
-          &lt; Back to catalog</div>
-        <div>
-          <h1>My Cart</h1>
-        </div>
-        <div>
-          {this.props.items.map(item => {
-            return (<CartSummaryItem
-              key={item.productId}
-              image={item.image}
-              name={item.name}
-              price={item.price}
-              shortDescription={item.shortDescription}
-            />);
-          })}
-        </div>
+      <div>
+        <h1>You have no items in your cart! Get shopping!</h1>
       </div>
     );
   }
